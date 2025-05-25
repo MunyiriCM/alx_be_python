53544ffabb58ecb2a62688795108d5722a17897b
MunyiriCM <mmunyiri12@gmail.com>
2025-05-25 21:45:15 +0300

    ALX Python proj

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1=10
+number2=5
+
+sum= number1+number2
+diff=number1-number2
+multx=number1*number2
+
+print(f"Sum of {number1} and {number2} is {sum}")
+print(f"Difference of {number1} and {number2} is {diff}")
+print(f"Product of {number1} and {number2} is {multx}")
